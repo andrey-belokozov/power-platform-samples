c07063769740edb5ea1225e15e837be5ab4a20e0
andrey-belokozov <andrey.belokozov.fl@gmail.com>
2024-08-08 14:23:54 +0500

    Delete send-keys-combination.ps1

--- send-keys-combination.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Load the System.Windows.Forms assembly
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-
-# Send keys combination (^ - sends CTRL, % - sends ALT, {q} - sends "q")
-[System.Windows.Forms.SendKeys]::SendWait("^%{q}")
